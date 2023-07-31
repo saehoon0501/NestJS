b3bafd538351a5c868603e7a7f11db1bb0be712b
saehoon0501 <76736828+saehoon0501@users.noreply.github.com>
2023-07-31 14:01:51 +0900

    TypeOrm config 수정

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,18 +1,19 @@
-import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from "@nestjs/typeorm";
 
 @Injectable()
 export class TypeOrmConfigService implements TypeOrmOptionsFactory {
   constructor(private configService: ConfigService) {}
 
   createTypeOrmOptions(): TypeOrmModuleOptions | Promise<TypeOrmModuleOptions> {
-    if (process.env.NODE_ENV === 'production') {
+    if (process.env.NODE_ENV === "production") {
       return {
-        type: 'postgres',
+        type: "postgres",
         synchronize: false,
-        database: this.configService.get<string>('DB_NAME'),
-        entities: ['**/*.entity.js'],
+        database: this.configService.get<string>("DB_NAME"),
+        entities: ["dist/**/*.entity.{ts,js}"],
+        migrations: [__dirname + "/migrations/*.ts"],
         migrationsRun: true,
         keepConnectionAlive: false,
         url: process.env.DATABASE_URL,
@@ -22,12 +23,14 @@ export class TypeOrmConfigService implements TypeOrmOptionsFactory {
       };
     }
     return {
-      type: process.env.NODE_ENV === 'production' ? 'postgres' : 'sqlite',
-      synchronize: process.env.NODE_ENV === 'test' ? true : false,
-      database: this.configService.get<string>('DB_NAME'),
+      type: "sqlite",
+      synchronize: true,
+      database: this.configService.get<string>("DB_NAME"),
       autoLoadEntities: true,
-      migrationsRun: process.env.NODE_ENV === 'test' ? true : false,
-      keepConnectionAlive: process.env.NODE_ENV === 'test' ? true : false,
+      migrationsRun: process.env.NODE_ENV === "test" ? true : false,
+      keepConnectionAlive: process.env.NODE_ENV === "test" ? true : false,
+      entities: ["dist/**/*.entity.{ts,js}"],
+      migrations: [__dirname + "/migrations/*.ts"],
     };
   }
 }
